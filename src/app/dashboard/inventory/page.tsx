2df9b9f2d9ccdd85f07dbf034bd700e3c17f5930
Manishankar Vakta <manishankarvakta@gmail.com>
2024-03-13 03:53:38 +0600

    Add damage, adjust nav in inventory page

--- src/app/dashboard/inventory/page.tsx
+++ src/app/dashboard/inventory/page.tsx
@@ -2,10 +2,18 @@ import PageTitle from "@/components/ui/PageTitle";
 import { UserDataTable } from "./data-table";
 import { columns } from "./columns";
 import prisma from "../../../../prisma";
-import { Toast } from "@/components/ui/toast";
+import {
+  Menubar,
+  MenubarContent,
+  MenubarItem,
+  MenubarMenu,
+  MenubarSeparator,
+  MenubarShortcut,
+  MenubarTrigger,
+} from "@/components/ui/menubar"
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
-import { ArrowLeftRight } from "lucide-react";
+import { ArrowLeftRight, GitCompareArrows, Trash } from "lucide-react";
 
 export default async function ProductsPage() {
   const data: any = []
@@ -17,6 +25,18 @@ export default async function ProductsPage() {
           <div className="flex items-center justify-between space-y-2">
             <PageTitle title="Inventory" />
             <div className="flex items-center space-x-2">
+              <Menubar>
+                <MenubarMenu>
+                  <MenubarTrigger>
+                    <Trash className="mr-2 h-3 w-3" />
+                    <Link href="/dashboard/damage"> Damage </Link>
+                  </MenubarTrigger>
+                </MenubarMenu>
+                <MenubarMenu>
+                  <MenubarTrigger><GitCompareArrows className="mr-2 h-3 w-3" /> <Link href="/dashboard/adjust">Adjustment</Link></MenubarTrigger>
+                </MenubarMenu>
+                
+              </Menubar>
               <Link href="/dashboard/movement">
                 <Button><ArrowLeftRight className="mr-2 h-4 w-4" /> Product Movement</Button>
               </Link>
